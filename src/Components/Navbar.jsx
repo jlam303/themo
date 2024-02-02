a504bd786ae3279b077946a983f8686b051e6099
jlam303 <110576725+jlam303@users.noreply.github.com>
2024-02-02 14:10:27 -0700

    Theme

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { useContext } from 'react'
+import { Them,handleThemo } from './ThemeContext'
+const Navbar = () => {
+    const val = useContext(Them)
+  return (
+    
+    <div className={val.themo}><button onClick={()=>{val.handleThemo()}}>Change Theme</button></div>
+  )
+}
+
+export default Navbar
---
+++ src/Components/ThemeContext.jsx
@@ -0,0 +1,28 @@
+import { createContext,useContext,useState } from 'react'
+import React from 'react'
+import Footer from "./Footer"
+import Header from "./Header"
+import Main from "./Main"
+import Navbar from "./Navbar"
+
+const Them = createContext();
+const ThemeContext = () => {
+  const [themo,setThemo] = useState("light");
+  const handleThemo = () => {
+    if(themo==="light"){
+        setThemo("dark")
+    }else{
+        setThemo("light")
+    }
+}
+  return (
+    <Them.Provider value={{handleThemo, themo}}>
+        <Navbar></Navbar>
+        <Header></Header>
+        <Main></Main>
+        <Footer></Footer>
+    </Them.Provider>
+  )
+}
+
+export {ThemeContext, Them}
